22666a27b0b031f867b99da8f692569fc7aa032f
Khinshan Khan <khanshan501@gmail.com>
2020-06-12 00:57:02 -0400

    looking into extracting and partitioning files for usefulness

--- assets/scripts/populate.js
+++ assets/scripts/populate.js
@@ -1,4 +1,12 @@
-const main = files => {
-  console.log(files);
-  document.getElementById("container").innerHTML = "changed";
+const reducer = files =>
+  files.reduce((stored, course) => {
+    const [course_num, course_info] = course.basename.split(/_(.+)/);
+    let course_data = stored[course_num] || {};
+    course_data[course_info] = course;
+    return { ...stored, [course_num]: course_data };
+  }, {});
+
+const main = (siteurl, files) => {
+  console.log(reducer(files));
+  document.getElementById("container").innerHTML = siteurl;
 };
