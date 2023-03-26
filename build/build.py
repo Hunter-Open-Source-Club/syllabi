88425d6da279d67ab8e30307ce3d480afe679c90
joshnatis <joshnatis0@gmail.com>
2023-03-25 20:31:25 -0400

    build.py: re-order functions for clarity

--- build/build.py
+++ build/build.py
@@ -5,6 +5,41 @@ import utils
 import config
 import syllabus
 
+def main():
+	html = open(config.HTML_TEMPLATE_PATH).read()
+	print(html.format(
+		title      = config.TITLE,
+		stylesheet = config.CSS_PATH,
+		favicon    = config.FAVICON_PATH,
+		script     = config.JS_PATH,
+		heading    = config.HEADING,
+		courses    = generate_course_html()
+	))
+
+def generate_course_html():
+	course_map = json.loads(open(config.COURSEMAP_PATH).read())
+	discover_syllabi_files(course_map)
+	return course_map_to_html(course_map)
+
+# augment course map with list of syllabi files from filesystem
+def discover_syllabi_files(course_map):
+	# schema of course map file: '{<category>:{<course_code>:<course_name>}}'
+	for category in course_map:
+		for course_code in course_map[category].copy():
+			course_path = config.SYLLABI_PATH + '/' + course_code.replace(' ', '_')
+			try: syllabi_files = os.listdir(course_path)
+			except FileNotFoundError:
+				course_map[category].pop(course_code)
+				continue
+
+			course_name = course_map[category][course_code]
+			course_map[category][course_code] = {
+				'course_name': course_code + ': ' + course_name,
+				'files': utils.sort_chronologically(syllabi_files)
+			}
+	# schema of post-processed course map file:
+	# '{<category>:{<course_code>:{"course_name":<course_name>,"files":[<files>]}}}'
+
 def course_map_to_html(course_map):
 	category_html = '<details open class="{classname}">{summary}<div class="content">{courses}</div></details>'
 	course_html   = '<details class="{classname}">{summary}{div}</details>'
@@ -46,36 +81,4 @@ def course_map_to_html(course_map):
 
 	return '\n'.join(categories)
 
-# augment course map with list of syllabi files from filesystem
-def discover_syllabi_files(course_map):
-	# schema of course map file: '{<category>:{<course_code>:<course_name>}}'
-	for category in course_map:
-		for course_code in course_map[category].copy():
-			course_path = config.SYLLABI_PATH + '/' + course_code.replace(' ', '_')
-			try: syllabi_files = os.listdir(course_path)
-			except FileNotFoundError:
-				course_map[category].pop(course_code)
-				continue
-
-			course_name = course_map[category][course_code]
-			course_map[category][course_code] = {
-				'course_name': course_code + ': ' + course_name,
-				'files': utils.sort_chronologically(syllabi_files)
-			}
-	# schema of post-processed course map file:
-	# '{<category>:{<course_code>:{"course_name":<course_name>,"files":[<files>]}}}'
-
-def generate_course_html():
-	course_map = json.loads(open(config.COURSEMAP_PATH).read())
-	discover_syllabi_files(course_map)
-	return course_map_to_html(course_map)
-
-html = open(config.HTML_TEMPLATE_PATH).read()
-print(html.format(
-	title      = config.TITLE,
-	stylesheet = config.CSS_PATH,
-	favicon    = config.FAVICON_PATH,
-	script     = config.JS_PATH,
-	heading    = config.HEADING,
-	courses    = generate_course_html()
-))
+main()
